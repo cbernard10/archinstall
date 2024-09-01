06f5083a5d6ffff6a06494798123681c07722edb
cbernard10 <clement.bernard@protonmail.com>
2024-09-01 19:31:54 +0200

    update

---
+++ archinstall.sh
@@ -0,0 +1,21 @@
+# start after partions have been written and formatted
+# and device is connected to the internet
+read -p "boot partition? " boot_p
+read -p "root partition? " root_p
+mount $root_p /mnt
+mount --mkdir $boot_p /mnt/boot
+pacstrap -K /mnt base linux linux-firmware linux-headers iwd nano dhcpcd xorg-server xorg-xrandr picom dunst polybar xorg-xinit unzip plasma-meta neovim ttf-hack-nerd gcc make npm ripgrep python-pywal plasma-meta bspwm sxhkd sudo
+genfstab -U >> /mnt/etc/fstab
+arch-chroot /mnt
+locale-gen
+echo LANG=en_US.UTF-8 > /etc/locale.conf
+echo KEYMAP=fr > /etc/vconsole.conf
+read -p "hostname? " hn
+echo $hn > /etc/hostname
+passwd
+grub-install --target=x86-64-efi --efi-directory=boot/ --bootloader-id=grub_uefi --recheck
+cp /usr/share/locale/en\@quot/LC_MESSAGES/grub.mo /boot/grub/locale/en.mo
+grub-mkconfig -o /boot/grub/grub.gfc
+exit
+umount -R /mnt
+reboot
