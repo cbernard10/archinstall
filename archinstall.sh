486b79cd1ecb5451c9240d85d2d77179584d1d1d
cbernard10 <clement.bernard@protonmail.com>
2024-09-01 20:25:01 +0200

    update

--- archinstall.sh
+++ archinstall.sh
@@ -5,18 +5,17 @@ read -p "boot partition? " boot_p
 read -p "root partition? " root_p
 mount $root_p /mnt
 mount --mkdir $boot_p /mnt/boot
-pacstrap -K /mnt base linux linux-firmware linux-headers iwd dhcpcd 
+pacstrap -K /mnt base linux linux-firmwareudo iwd dhcpcd 
 genfstab -U /mnt >> /mnt/etc/fstab
-arch-chroot /mnt
-locale-gen
-echo LANG=en_US.UTF-8 > /etc/locale.conf
-echo KEYMAP=fr > /etc/vconsole.conf
-read -p "hostname? " hn
-echo $hn > /etc/hostname
-passwd
-grub-install --target=x86-64-efi --efi-directory=boot/ --bootloader-id=grub_uefi --recheck
-cp /usr/share/locale/en\@quot/LC_MESSAGES/grub.mo /boot/grub/locale/en.mo
-grub-mkconfig -o /boot/grub/grub.gfc
-exit
+arch-chroot /mnt locale-gen
+arch-chroot /mnt echo LANG=en_US.UTF-8 > /etc/locale.conf
+arch-chroot /mnt echo KEYMAP=fr > /etc/vconsole.conf
+arch-chroot /mnt read -p "hostname? " hn
+arch-chroot /mnt echo $hn > /etc/hostname
+arch-chroot /mnt passwd
+arch-chroot /mnt grub-install --target=x86-64-efi --efi-directory=boot/ --bootloader-id=grub_uefi --recheck
+arch-chroot /mnt cp /usr/share/locale/en\@quot/LC_MESSAGES/grub.mo /boot/grub/locale/en.mo
+arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.gfc
+arch-chroot /mnt exit
 umount -R /mnt
 reboot
