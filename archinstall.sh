c3553b1270b04dbe268d6cf4fff869a3e99fa2be
cbernard10 <clement.bernard@protonmail.com>
2024-09-01 20:56:46 +0200

    update

--- archinstall.sh
+++ archinstall.sh
@@ -15,7 +15,7 @@ arch-chroot /mnt echo $hn > /etc/hostname
 arch-chroot /mnt passwd
 arch-chroot /mnt grub-install --target=x86_64-efi --efi-directory=boot/ --bootloader-id=grub_uefi --recheck
 arch-chroot /mnt cp /usr/share/locale/en\@quot/LC_MESSAGES/grub.mo /boot/grub/locale/en.mo
-arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.gfc
+arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
 #arch-chroot /mnt exit 
 #umount -R /mnt
 #reboot
