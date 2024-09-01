b9efe12fee81619b9d09ee05f9fb5e9664f2bd6e
cbernard10 <clement.bernard@protonmail.com>
2024-09-02 01:37:22 +0200

    update

--- bspwm-install.sh
+++ bspwm-install.sh
@@ -3,5 +3,20 @@ sudo pacman -S --needed git base-devel
 git clone https://aur.archlinux.org/yay.git
 cd yay 
 makepkg -si
+cd ..
 
 sudo pacman -S alacritty bspwm btop neofetch neovim picom polybar ranger sxhkd dunst xorg-server xorg-xinit xorg-xrandr ttf-hack-nerd python-pywal tmux nvidia
+
+echo "sxhkd &; exec bspwm" > .xinitrc
+
+mkdir .config/bspwm
+mkdir .config/sxhkd
+mkdir .config/alacritty
+
+git clone https://github.com/cbernard10/dotfiles
+
+cp dotfiles/bspwm/bspwmrc .config/bspwm/
+cp dotfiles/sxhkd/sxhkdrc .config/sxhkd/
+cp dotfiles/alacritty/alacritty.toml .config/alacritty/
+
+sudo reboot now
