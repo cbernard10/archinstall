8b4246d154736b610ef529c1d0fe1c49d333be68
cbernard10 <clement.bernard@protonmail.com>
2024-09-01 21:14:37 +0200

    update

--- archinstall.sh
+++ archinstall.sh
@@ -4,6 +4,7 @@
 read -p "boot partition? " boot_p
 read -p "root partition? " root_p
 read -p "hostname? " hn
+read -p "username? " un
 mount $root_p /mnt
 mount --mkdir $boot_p /mnt/boot
 pacstrap -K /mnt base linux linux-firmware linux-headers sudo iwd dhcpcd efibootmgr grub intel-ucode
@@ -16,8 +17,11 @@ arch-chroot /mnt passwd
 arch-chroot /mnt grub-install --target=x86_64-efi --efi-directory=boot/ --bootloader-id=grub_uefi --recheck
 arch-chroot /mnt cp /usr/share/locale/en\@quot/LC_MESSAGES/grub.mo /boot/grub/locale/en.mo
 arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
+arch-chroot /mnt useradd -m $un
+arch-chroot /mnt passwd $un
 arch-chroot /mnt pacman -Syu
 arch-chroot /mnt pacman -S plasma-meta bspwm sxhkd xorg-server xorg-xinit xorg-xrandr picom dunst polybar 
+arch-chroot /mnt echo "export DESKTOP_SESSION=plasma; exec startplasma-x11" > "/home/$un/.xinitrc"
 #arch-chroot /mnt exit 
 #umount -R /mnt
 #reboot
